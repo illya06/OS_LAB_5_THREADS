10aa8c5c60ea0e76190d57472a77c8495bcd2465
illya06 <47461573+illya06@users.noreply.github.com>
2020-12-06 21:54:54 +0200

    Added work for threads

--- OS_LAB_5_THREADS/OS_LAB_5_THREADS.cpp
+++ OS_LAB_5_THREADS/OS_LAB_5_THREADS.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+void task11(double step, double left, double right);
+
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
+//Tabulation of (1 + x)^(1/3)
+void task11(double step, double left, double right) {
+    int x;
+    for (int i = left; i < right; i += step) {
+        x = 1 + x / 3 - x * x / 9 + x * x * x * 5 / 81 - x * x * x * x * 80 / 1944;
+    }
+}
