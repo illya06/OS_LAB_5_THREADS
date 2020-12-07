b7963fa547d32254f4dc2ea4ca16f74d329a4b3e
illya06 <47461573+illya06@users.noreply.github.com>
2020-12-07 20:27:57 +0200

    skrewed interface to control panel

--- OS_LAB_5_THREADS/OS_LAB_5_THREADS.cpp
+++ OS_LAB_5_THREADS/OS_LAB_5_THREADS.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -17,13 +18,17 @@ DWORD WINAPI test(LPVOID lparam) {
 //ass
 void createThreads(int ammount);
 void closeHandles(int ammount);
-//asdasdsa
+void runControls();
 
 //----------------------
 //GLOBAL VARIABLES
-HANDLE threads[16];
-DWORD  threadID[16];
+HANDLE  threads[16];
+DWORD   threadID[16];
 double* dataArray[16];
+
+string              program = "C:\\Users\\illyich_\\source\\repos\\OS_LAB_5_CONTOLS\\Debug\\OS_LAB_5_CONTOLS.exe";
+STARTUPINFO         si;
+PROCESS_INFORMATION pi;
 //----------------------
 
 
@@ -33,6 +38,7 @@ int main()
     cout << "ENTER AMMOUNT OF THREADS : ";
     cin >> n;
     createThreads(n);
+    runControls();
     WaitForMultipleObjects(n, threads, TRUE, INFINITE);
     //task11(0.001, -0.5, 0.5);
     closeHandles(n);
@@ -50,6 +56,41 @@ DWORD WINAPI task11(double step, double left, double right) {
     return 0;
 }
 
+void runControls() {
+    string temp_prog = program;
+    for(DWORD id : threadID) {
+        if (id != 0) {
+            temp_prog += " ";
+            temp_prog += to_string(id);
+        }
+    }
+
+    int bufferlen = ::MultiByteToWideChar(CP_ACP, 0, temp_prog.c_str(), temp_prog.size(), NULL, 0);
+
+    //SOME SHITTY CODE FOR STR TO LPWSTR CONVERTION
+    LPWSTR widestr = new WCHAR[bufferlen + 1];
+    ::MultiByteToWideChar(CP_ACP, 0, temp_prog.c_str(), temp_prog.size(), widestr, bufferlen);
+    widestr[bufferlen] = 0;
+
+    if (!CreateProcess(
+        NULL,            // Module name 
+        widestr,               // Command line
+        NULL,               // Process handle inheritance
+        NULL,               // Thread handle
+        FALSE,              // Handle inheritance
+        CREATE_NEW_CONSOLE, // Flags
+        NULL,               // Use parent's environment block
+        NULL,               // Use parent's starting directory 
+        &si,             // Pointer to STARTUPINFO
+        &pi)             // Pointer to PROCESS_INFORMATION
+        )
+    {
+        printf("FAILED to createProcess. ERROR_CODE [%d]\n", GetLastError());
+        Sleep(5000);
+        return;
+    }
+}
+
 void createThreads(int ammount) {
     for (int i = 0; i < ammount; i++) {
         threads[i] = CreateThread(
@@ -57,7 +98,7 @@ void createThreads(int ammount) {
             0,
             test,
             dataArray[i],
-            CREATE_NEW_CONSOLE,
+            CREATE_SUSPENDED,
             &threadID[i]
         );
         if (threads[i] == NULL)
